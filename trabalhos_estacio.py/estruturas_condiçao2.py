0ad3f211412899ced321413c19108ef54b4a0388
Fabriciober <95449812+Fabriciober@users.noreply.github.com>
2024-10-01 21:31:02 -0300

    Update estruturas_condiçao2.py

--- trabalhos_estacio.py/estruturas_condiçao2.py
+++ trabalhos_estacio.py/estruturas_condiçao2.py
@@ -1,7 +1,7 @@
 tempoexperiencia = 1
 if tempoexperiencia <2:
     print('nivel de conhecimento junior')
-elif tempoexperiencia <5:
+elif tempoexperiencia >2 and tempoexperiencia <5:
     print('nivel de cohecimento pleno')
 else:
     print('nivel de conhecimento senior')    
